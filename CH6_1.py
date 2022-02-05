849c4559b39f6aa30e1aa4e0713fa3385a1bc644
JunHwi-Jeon <97847752+JunHwi-Jeon@users.noreply.github.com>
2022-02-05 23:38:26 +0900

    Add files via upload

---
+++ CH6_1.py
@@ -0,0 +1,88 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import softmax, sigmoid, cross_entropy_error
+from common.gradient import numerical_gradient
+from dataset.mnist import load_mnist
+from ch05.two_layer_net import TwoLayerNet
+
+class SGD:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+        
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+            
+class Momentum:
+    def __init__(self, lr = 0.01, momentum = 0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+        
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
+
+class AdaGrad:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+                
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+            
+            
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+optimizer = SGD()
+
+## hyperparameter
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    
+    # to get minibatch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    # to calculate gradient
+    # back propagation
+    grads = network.gradient(x_batch, t_batch)
+    params = network.params
+    optimizer.update(params, grads)
+        
+    # record learing
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    
+    # calculate accuracy per 1 epoch
+    if i % 100 == 0: #iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(str(i) + " train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
