919c54619a5d653a2a6fe21547ff92f6ea3c17f4
Plusholic <97847752+Plusholic@users.noreply.github.com>
2022-03-25 23:51:33 +0900

    Add files via upload

---
+++ pytorch_practice_08_RNN.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import os
+
+df = pd.read_csv("./pytorch_sample/data/kospi.csv")
+scaler = MinMaxScaler()
+df[['Open', 'High', 'Low', 'Close', 'Volume']] = scaler.fit_transform(df[['Open', 'High', 'Low', 'Close', 'Volume']])
+
+print(df.head())
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+X = df[['Open', 'High', 'Low', 'Volume']].values
+y = df['Close'].values
+
+def seq_data(x,y,sequence_length):
+    x_seq = []
+    y_seq = []
+    for i in range(len(x) - sequence_length):
+        x_seq.append(x[i:i+sequence_length])
+        y_seq.append(y[i+sequence_length])
+        
+    return torch.FloatTensor(x_seq).to(device), torch.FloatTensor(y_seq).to(device).view(-1, 1)
+
+split = 200
+sequence_length = 5
+x_seq, y_seq = seq_data(X, y, sequence_length)
+x_train_seq = x_seq[:split]
+y_train_seq = y_seq[:split]
+x_test_seq = x_seq[split:]
+y_test_seq = y_seq[split:]
+print(x_train_seq.size(), y_train_seq.size())
+print(x_test_seq.size(), y_test_seq.size())
+
+
+train = torch.utils.data.TensorDataset(x_train_seq, y_train_seq)
+test = torch.utils.data.TensorDataset(x_test_seq, y_test_seq)
+batch_size = 20
+train_loader = torch.utils.data.DataLoader(dataset=train, batch_size=batch_size, shuffle=True)
+
+print(x_seq.size())
+input_size = x_seq.size(2)
+num_layers = 2
+hidden_size = 8
+
+class VanillaRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, sequence_length, num_layers, device):
+        super(VanillaRNN, self).__init__()
+        self.device = device
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Sequential(nn.Linear(hidden_size*sequence_length, 1), nn.Sigmoid())
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size()[0],self.hidden_size).to(self.device)
+        out,_ = self.rnn(x, h0)
+        out = out.reshape(out.shape[0], -1)
+        out = self.fc(out)
+        return out
+    
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, sequence_length, num_layers, device):
+        super(LSTM, self).__init__()
+        self.device = device
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Sequential(nn.Linear(hidden_size*sequence_length, 1), nn.Sigmoid())
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size()[0],self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size()[0],self.hidden_size).to(self.device)
+        out,_ = self.lstm(x, (h0, c0))
+        out = out.reshape(out.shape[0], -1)
+        out = self.fc(out)
+        return out
+    
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, sequence_length, num_layers, device):
+        super(GRU, self).__init__()
+        self.device = device
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Sequential(nn.Linear(hidden_size*sequence_length, 1), nn.Sigmoid())
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size()[0],self.hidden_size).to(self.device)
+        out,_ = self.gru(x, h0)
+        out = out.reshape(out.shape[0], -1)
+        out = self.fc(out)
+        return out
+
+model = GRU(input_size = input_size,
+                   hidden_size = hidden_size,
+                   sequence_length = sequence_length,
+                   num_layers = num_layers,
+                   device = device).to(device)
+
+criterion = nn.MSELoss()
+num_epochs = 301
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+loss_graph = []
+n = len(train_loader)
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    
+    for data in train_loader:
+        
+        seq, target = data
+        out = model(seq)
+        loss = criterion(out, target)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        
+    loss_graph.append(running_loss/n)
+    if epoch % 100 == 0:
+        print('[epoch:%d] loss: %.4f'%(epoch, running_loss/n))
+        
+        
+plt.figure(figsize=(20,10))
+plt.plot(loss_graph)
+plt.show()
+
+concatdata = torch.utils.data.ConcatDataset([train, test])
+data_loader = torch.utils.data.DataLoader(dataset=concatdata,batch_size=100)
+with torch.no_grad():
+    pred = []
+    model.eval()
+    for data in data_loader:
+        seq, target = data
+        out = model(seq)
+        pred += out.cpu().tolist()
+
+plt.figure(figsize=(20,10))
+plt.plot(np.ones(100)*len(train), np.linspace(0,1,100),'--',linewidth=0.6)
+plt.plot(df['Close'][sequence_length:].values,'--')
+plt.plot(pred,'b',linewidth=0.6)
+plt.legend(['train boundary', 'actual', 'prediction'])
+plt.show()
